6012d427665b419710d9fe85bd535f7eb9fa9444
Ele-nonsochi <elea.luzi25@gmail.com>
2021-10-27 16:58:02 +0200

    Esce ciò che richiesto dall' esercizio

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 //Creo una funzione che mi crea 5 numeri random 
 
 const arrayNumber = randomNumber();
+let numberTime;
 
 function randomNumber() {
     const numberGenerate = [];
@@ -23,4 +24,36 @@ function randomNumber() {
 
 alert(arrayNumber);
 
-//
+//Creo un timing di 30 secondi, prima che il prompt richieda al'utente di inserire i numeri 
+
+setTimeout(function () {
+    numberTime = prompt("Inserisci cinque numeri separati");
+    let userArray = [];
+
+    if (numberTime.includes(" "))
+        userArray = numberTime.split(" ")
+    else
+        userArray = numberTime.split(",")
+
+    //Controllo che siamo dei numeri 
+    for (let i = 0; i < 5; i++) {
+        userArray[i] = parseInt(userArray[i])
+    }
+
+
+    //Controllo quali e quanti numeri sono stati generati
+
+    let resulFinal = [];
+    let counter = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const typeNumber = userArray[i]
+        if (arrayNumber.includes(typeNumber)) {
+            resulFinal.push(typeNumber)
+            counter++;
+        }
+    }
+
+    alert(`Hai indovinato ${counter} numeri. I numeri indovinati sono: ${resulFinal}`)
+}, 3000)
+
